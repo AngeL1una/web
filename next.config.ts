cbbe7ce8e5adfdcc2d244e3972ed224aa6cb731c
Angel Luna Lugo <131845927+AngeL1una@users.noreply.github.com>
2025-03-18 23:23:05 -0600

    Forzar modo claro en la aplicación

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'png.pngtree.com'], // Agrega aquí los dominios de las imágenes externas
   },
+  
 };
 
 module.exports = nextConfig;
